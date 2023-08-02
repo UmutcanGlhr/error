3d17addb99d0bb6065b9ed2ecbc2d27cf69436d8
Umutcan <97991265+UmutcanGlhr@users.noreply.github.com>
2023-08-02 17:18:30 +0300

    bug fixed

--- WindowsService1/WindowsService1/Service1.cs
+++ WindowsService1/WindowsService1/Service1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.ServiceProcess;
@@ -27,11 +28,11 @@ namespace WindowsService1
             {
                 timer = new Timer();
             }
-            timer.Interval = 2000;//86400000
+            timer.Interval = 120000;//86400000
             timer.Elapsed += Timer_Elapsed;
             timer.Enabled = true;
             timer.Start();
-            System.Diagnostics.Debugger.Launch();
+            
         }
 
         protected override void OnStop()
@@ -41,21 +42,22 @@ namespace WindowsService1
         private void Timer_Elapsed(object sender, ElapsedEventArgs e)
         {
             timer.Stop();
-        
+            Task.Run(() => Update_PriceAsync());
+            
             timer.Start();
         }
 
        
             
-            public async Task Update_PriceAsync()
+            public async void Update_PriceAsync()
             {
-                NpgsqlConnection connectionString = new NpgsqlConnection("server=localHost;port=5432;Database=updateprice;user ID=postgres;password=1234;");
+                NpgsqlConnection connectionString = new NpgsqlConnection("server=localHost;port=5432;Database=Solution1;user ID=postgres;password=1234;");
                 DateTime time = DateTime.Now.AddDays(1);
                 string format = "yyy-MM-dd";
                 string tarih = time.ToString(format);
-                //Console.WriteLine(tarih);
+            //Console.WriteLine(tarih);
 
-                string url = $"https://seffaflik.epias.com.tr/transparency/service/market/mcp-average?startDate={tarih}&endDate={tarih}&period=DAILY";
+            string url = $"https://seffaflik.epias.com.tr/transparency/service/market/mcp-average?startDate={tarih}&endDate={tarih}&period=DAILY";
                 //Console.WriteLine(url);
                 try
                 {
@@ -66,24 +68,54 @@ namespace WindowsService1
                         if (response.IsSuccessStatusCode)
                         {
                             string fiyat;
-                            string day;
+                            DateTime day;
                             string content = await response.Content.ReadAsStringAsync();
                             dynamic data = JsonConvert.DeserializeObject(content);
 
                             foreach (var item in data.body.dayAheadMCPList)
                             {
-                                fiyat = Convert.ToString(item.averageMcp);
-                                day = Convert.ToDateTime(item.date);
-                                connectionString.Open();
-                                string queryStr = "Update \"price\" set \"fiyat\" = @fiyat , \"tarih\"=@tarih";
+                            fiyat = Convert.ToString(item.averageMcp);
+                            day = Convert.ToDateTime(item.date);
+                            File.AppendAllText("log.txt", fiyat);
+                            File.AppendAllText("log.txt", day.ToString());
 
+                            try
+                            {
+                                connectionString.Open();
+                                File.AppendAllText("log.txt", "bağlantı başarılı");
+                            }
+                            catch (Exception)
+                            {
+
+                                throw;
+                            }
+                            try
+                            {
+                                string queryStr = "Update \"Fiyatlar\" set \"Fiyat\" = @fiyat , \"Tarih\"=@tarih";
+                                Console.WriteLine(fiyat + " " + day);
                                 NpgsqlCommand cmd = new NpgsqlCommand(queryStr, connectionString);
                                 cmd.Parameters.AddWithValue("@fiyat", fiyat);
                                 cmd.Parameters.AddWithValue("@tarih", day);
                                 cmd.ExecuteNonQuery();
                                 cmd.Dispose();
-                                connectionString.Close();
+                                File.AppendAllText("log.txt", "ekleme başarılı");
                             }
+                            catch (Exception)
+                            {
+
+                                throw;
+                            }
+                            try
+                            {
+                                connectionString.Close();
+                                File.AppendAllText("log.txt", "çıkış yapıldı");
+                            }
+                            catch (Exception)
+                            {
+
+                                throw;
+                            }
+                        }
 
                         }
                         else
